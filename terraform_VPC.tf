fab42d5f9ce73b562998c1960ce8c80e7d96cc87
Shamadansari123 <100352782+Shamadansari123@users.noreply.github.com>
2025-05-23 13:33:29 +0530

    Update terraform_VPC

--- terraform_VPC.tf
+++ terraform_VPC.tf
@@ -98,12 +98,19 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "web" {
   ami                         = "ami-0c02fb55956c7d316" 
-  instance_type               = "t2.medium"
+  instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
-  
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
   tags = {
     Name = "web-server"
   }
